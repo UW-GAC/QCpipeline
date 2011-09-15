5e17ac6af291d287eec2f12bef9a464be2b9dcc1
Stephanie M. Gogarten <sdmorris@uw.edu>
2011-09-15 21:21:16 +0000

    added missing call rate

--- README.txt
+++ README.txt
@@ -1,13 +1,14 @@
 QC Pipeline
+Step numbers refer to the SOP
 
 1) Create project directory
 create_project_dir.sh ProjectName user
 (where "user" is the first analyst working on the project)
 
-2) Create scan and snp annotation files
+2-8) Create scan and snp annotation files
 (save as ScanAnnotationDataFrame and SnpAnnotationDataFrame)
 
-3) Create NetCDF and GDS files
+9-10) Create NetCDF and GDS files
 test 5 samples first:
 python netcdf.py --email user@uw.edu --test ncdf.config
 (where "user@uw.edu" is your email address)
@@ -16,5 +17,8 @@ run all:
 python netcdf.py --email user@uw.edu ncdf.config
 Check output to make sure creation was successful
 
-4) Gender check (heterozygosity and mean intensity)
+13-15) Gender check (heterozygosity and mean intensity)
 python gender_check.py --email user@uw.edu gender.config
+
+19-20) Missing call rate
+python missing.py --email user@uw.edu missing.config
---
+++ missing.R
@@ -0,0 +1,139 @@
+##########
+# Missing call rates by SNP and scan
+# Usage: R --args config.file < missing.R
+##########
+
+library(GWASTools)
+sessionInfo()
+
+# read configuration
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) < 1) stop("missing configuration file")
+config.table <- read.table(args[1], as.is=TRUE)
+config <- config.table[,2]
+names(config) <- config.table[,1]
+
+(scanAnnot <- getobj(config["annot_scan_file"]))
+scanID <- getScanID(scanAnnot)
+
+(snpAnnot <- getobj(config["annot_snp_file"]))
+snpID <- getSnpID(snpAnnot)
+
+ncfile <- config["nc_geno_file"]
+nc <- NcdfGenotypeReader(ncfile)
+genoData <- GenotypeData(nc, scanAnnot=scanAnnot, snpAnnot=snpAnnot)
+
+# are there any scans to exclude?
+if (!is.na(config["scan_exclude_file"])) {
+  scan.exclude <- getobj(config["scan_exclude_file"])
+  stopifnot(all(scan.exclude %in% scanID))
+} else {
+  scan.exclude <- NULL
+}
+length(scan.exclude)
+
+# missing.n1
+miss.by.snp <- missingGenotypeBySnpSex(genoData, scan.exclude=scan.exclude)
+save(miss.by.snp, file=config["out_n1_file"])
+missing.n1 <- miss.by.snp$missing.fraction
+stopifnot(allequal(names(missing.n1), snpID))
+
+# summary table
+chr.type <- getChromosome(snpAnnot, char=TRUE)
+chr.type[is.element(chr.type, 1:22)] <- "A"
+np <- table(chr.type)
+
+# fraction of intensity only per chrom
+snp.io <- getVariable(snpAnnot, config["annot_snp_IntensityOnlyCol"])
+if (!is.null(snp.io)) {
+  xm <- as.matrix(table(chr.type, snp.io))
+  io <- xm[,"1"] / rowSums(xm)
+  nonio <- snp.io == 0
+} else {
+  nonio <- rep(TRUE, nrow(snpAnnot))
+}
+
+# fraction of non-intensity only snps that are tech failures
+xm <- as.matrix(table(chr.type[nonio], missing.n1[nonio] == 1))
+tf <- xm[,"TRUE"] / rowSums(xm)
+
+# fraction of snps with missing.n1>0.05 among passed SNPs
+goodsnp <- nonio & missing.n1 < 1
+xm <- as.matrix(table(chr.type[goodsnp], missing.n1[goodsnp]>0.05))
+mf <- xm[,"TRUE"] / rowSums(xm)
+
+if (!is.null(snp.io)) {
+  snptbl <- rbind(np, io, tf, mf)
+  row.names(snptbl) <- c("number of probes", "intensity-only", "SNP tech failures", "missing>0.05")
+} else {
+  snptbl <- rbind(np, tf, mf)
+  row.names(snptbl) <- c("number of probes", "SNP tech failures", "missing>0.05")
+}
+save(snptbl, file=config["out_snp_summary"])
+
+
+# missing.e1 - snps to exclude
+snp.exclude <- snpID[missing.n1 == 1]
+length(snp.exclude)
+
+# missing.e1
+miss.by.scan <- missingGenotypeByScanChrom(genoData, snp.exclude=snp.exclude)
+missing.e1 <- miss.by.scan$missing.fraction
+stopifnot(allequal(names(missing.e1), scanID))
+
+# autosomal
+auto <- colnames(miss.by.scan$missing.counts) %in% 1:22
+missa <- rowSums(miss.by.scan$missing.counts[,auto]) /
+  sum(miss.by.scan$snps.per.chr[auto])
+stopifnot(allequal(names(missa), scanID))
+miss.by.scan$missing.fraction.auto <- missa
+
+# X chromosome
+missx <- miss.by.scan$missing.counts[,"X"] / miss.by.scan$snps.per.chr["X"]
+stopifnot(allequal(names(missx), scanID))
+miss.by.scan$missing.fraction.xchr <- missx
+save(miss.by.scan, file=config["out_e1_file"])
+
+
+# do round 2?
+if (as.logical(config["round2"])) {
+  # are there any scans with missing.e1 >= 0.05?
+  badscans <- missing.e1 >= 0.05
+  
+  # missing.n2
+  if (any(badscans)) {
+    scan.exclude <- unique(c(scan.exclude, scanID[badscans]))
+    print(length(scan.exclude))
+    
+    miss.by.snp <- missingGenotypeBySnpSex(genoData, scan.exclude=scan.exclude)
+    save(miss.by.snp, file=config["out_n2_file"])
+    missing.n2 <- miss.by.snp$missing.fraction
+    stopifnot(allequal(names(missing.n2), snpID))
+  } else {
+    missing.n2 <- missing.n1
+  }
+
+  snp.exclude <- snpID[missing.n2 >= 0.05]
+  print(length(snp.exclude))
+  
+  # missing.e2
+  miss.by.scan <- missingGenotypeByScanChrom(genoData, snp.exclude=snp.exclude)
+  missing.e2 <- miss.by.scan$missing.fraction
+  stopifnot(allequal(names(missing.e2), scanID))
+  
+  # autosomal
+  auto <- colnames(miss.by.scan$missing.counts) %in% 1:22
+  missa <- rowSums(miss.by.scan$missing.counts[,auto]) /
+  sum(miss.by.scan$snps.per.chr[auto])
+  stopifnot(allequal(names(missa), scanID))
+  miss.by.scan$missing.fraction.auto <- missa
+  
+  # X chromosome
+  missx <- miss.by.scan$missing.counts[,"X"] / miss.by.scan$snps.per.chr["X"]
+  stopifnot(allequal(names(missx), scanID))
+  miss.by.scan$missing.fraction.xchr <- missx
+  save(miss.by.scan, file=config["out_e2_file"])
+
+}
+
+close(genoData)
