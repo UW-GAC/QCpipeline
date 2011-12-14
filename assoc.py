43975c2cbd82c20f1c8f2549724873a5deb2ac76
Jess Shen <j7shen@uw.edu>
2011-12-14 02:49:43 +0000

    user can specify the queue for association

--- assoc.py
+++ assoc.py
@@ -38,6 +38,8 @@ parser.add_option("--plotQQManh", dest="plotqq",
 parser.add_option("--plotClust", dest="plotcl",
                   action="store_true", default=False,
                   help="cluster plots")
+parser.add_option("--queue", dest="qname",
+                  default="gcc.q", help="cluster plots")
 (options, args) = parser.parse_args()
 
 config = args[0]
@@ -50,6 +52,7 @@ assoc = options.assoc
 merge = options.merge
 plotq = options.plotqq
 plotc = options.plotcl
+qname = options.qname
 
 # 3 arguments (config file, starting chrom,and end chrom) when assoc = True
 #if assoc:
@@ -88,15 +91,15 @@ if assoc:
         sys.exit("cEnd is smaller than cStart")
 
     if (cStart == 26 & cEnd == 26):
-       jobid[job+".26"] = QCpipeline.submitJob(job+".chrom26", driver, [rscript, config, "26"], queue="gcc.q", email=email)
+       jobid[job+".26"] = QCpipeline.submitJob(job+".chrom26", driver, [rscript, config, "26"], queue=qname, email=email)
     elif (cEnd < 25 | (cEnd >= 25 & (not covarsex))): # no need to single out y if cEnd is lt 25 or covarsex = False
         for ichrom in chroms:
-            jobid[job+"."+str(ichrom)] = QCpipeline.submitJob(job+".chrom"+str(ichrom), driver, [rscript, config, str(ichrom)], queue="gcc.q", email=email)
+            jobid[job+"."+str(ichrom)] = QCpipeline.submitJob(job+".chrom"+str(ichrom), driver, [rscript, config, str(ichrom)], queue=qname, email=email)
     else: # run y without sex 
-        jobid[job+".25"] = QCpipeline.submitJob(job+".chrom25", driver, [rscript, config, "25", sex], queue="gcc.q", email=email)
+        jobid[job+".25"] = QCpipeline.submitJob(job+".chrom25", driver, [rscript, config, "25", sex], queue=qname, email=email)
         del chroms[chroms.index(25)]
         for ichrom in chroms:
-            jobid[job+"."+str(ichrom)] = QCpipeline.submitJob(job+".chrom"+str(ichrom), driver, [rscript, config, str(ichrom)], queue="gcc.q", email=email)
+            jobid[job+"."+str(ichrom)] = QCpipeline.submitJob(job+".chrom"+str(ichrom), driver, [rscript, config, str(ichrom)], queue=qname, email=email)
     #print jobid
 
 
