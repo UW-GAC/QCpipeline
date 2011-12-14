8355f523b8cef9600f722ccc209502c2dccab156
Stephanie M. Gogarten <sdmorris@uw.edu>
2011-12-14 19:47:34 +0000

    added dbgap annotation functions to QCpipeline

--- QCpipeline/R/dbgapAnnotation.R
+++ QCpipeline/R/dbgapAnnotation.R
@@ -1,24 +1,18 @@
 # make sample annotation files for dbGaP
 # arguments:
-#  annot - dataframe with sample annotation
-#  vardata - dataframe with columns "varname", "newname", "annotation",
-#     "analysis," "description", "type"
+#  annot - ScanAnnotationDataFrame
 #  dir - directory for output (default current)
 
-dbgap.sample.annotation <- function(annot, vardata, dir=".") {
+dbgapScanAnnotation <- function(scanAnnot, dir=".",
+   consentVar="consent", subjVar="subj.plink", dupVar="dup.post", omitVar="no.post",
+   annotationCol="dbGaP.annot", analysisCol="dbGaP.anal") {
 
-  # check for essential columns
-  stopifnot("consent" %in% names(annot),
-            "subj.plink" %in% names(annot),
-            "dup.post" %in% names(annot),
-            "no.post" %in% names(annot))
-  stopifnot("consent" %in% vardata$varname,
-            "subj.plink" %in% vardata$varname,
-            "dup.post" %in% vardata$varname,
-            "no.post" %in% vardata$varname)
+  stopifnot(is(scanAnnot, "ScanAnnotationDataFrame"))
+  stopifnot(all(hasVariable(scanAnnot, c(consentVar, subjVar, dupVar, omitVar))))
+  stopifnot(all(c(annotationCol, analysisCol) %in% names(varMetadata(scanAnnot))))
   
   # consent levels
-  conslev <- unique(annot$consent)
+  conslev <- unique(scanAnnot[[consentVar]])
 
   # annotation and analysis
   for (type in c("annotation", "analysis")) {
@@ -31,42 +25,41 @@ dbgap.sample.annotation <- function(annot, vardata, dir=".") {
     consent.dupfiles <- paste(dir, "/Sample_", type, "_duplicates_consent_", conslev, ".csv", sep="")
 
     # variables to include in sample annotation file
-    sann <- vardata$varname[vardata[,type] == 1]
-    nsann <- vardata$newname[vardata[,type] == 1]
+    if (type == "annotation") thistype <- annotationCol else thistype <- analysisCol
+    sann <- unique(varLabels(scanAnnot)[varMetadata(scanAnnot)[[thistype]]],
+                   c(consentVar, subjVar, dupVar, omitVar))
 
     # remove any samples that will not be posted on dbGaP
-    annot2 <- annot[!annot$no.post, sann]
-
-    # change var names to a more generic form
-    names(annot2) <- nsann
+    annot2 <- scanAnnot[!scanAnnot[[omitVar]], sann]
 
     # split the sample annotation into the main, unduplicated set
     # (one sample per subject) and the duplicated set
-    subj <- annot2[annot2$subj.plink,]
-    dups <- annot2[annot2$dup.post,]
+    subj <- annot2[annot2[[subjVar]],]
+    dups <- annot2[annot2[[dupVar]],]
     stopifnot(nrow(dups) + nrow(subj) == nrow(annot2))
   
     # remove the splitting variable(s)
-    x <- which(is.element(names(subj),c("subj.plink","dup.post","no.post")))
-    subj <- subj[,-x] 
-    x <- which(is.element(names(dups),c("subj.plink","dup.post","no.post")))
-    dups <- dups[,-x]
+    subj <- subj[,!(varLabels(subj) %in% c(subjVar, dupVar, omitVar))] 
+    dups <- dups[,!(varLabels(dups) %in% c(subjVar, dupVar, omitVar))]
 
     # prepare data dictionary
-    dd <- vardata[vardata[,type] == 1, c("newname", "type", "description")]
-    dd <- dd[!is.element(dd$newname,c("subj.plink","dup.post","no.post")),]
-    stopifnot(names(subj) == dd$newname)
-    names(dd)[1] <- "variable"
+    meta <- varMetadata(subj)[varMetadata(subj)[[thistype]],]
+    meta$type <- unlist(lapply(pData(subj), class))
+    meta$variable <- row.names(meta)
+    dd <- meta[,c("variable", "labelDescription", "type")]
+    names(dd) <- c("variable", "description", "type")
 
     # write the files
+    subj <- pData(subj)
     write.csv(subj, file=annotfile, quote=FALSE, row.names=FALSE)
+    dups <- pData(dups)
     write.csv(dups, file=dupfile, quote=FALSE, row.names=FALSE)
     write.table(dd, file=ddfile, sep="\t", quote=FALSE, row.names=FALSE)
 
     # Divide samples by consent group
     if (type == "annotation") {
       for (i in 1:length(conslev)) {
-        s <- subj[subj$consent == conslev[i],]
+        s <- subj[subj[[consentVar]] == conslev[i],]
         write.csv(s, file=consent.annotfiles[i], quote=FALSE, row.names=FALSE)
       }
 
@@ -81,34 +74,35 @@ dbgap.sample.annotation <- function(annot, vardata, dir=".") {
 
 # make snp annotation files for dbGaP
 # arguments:
-#  annot - dataframe with snp annotation
-#  vardata - dataframe with columns "varname", "newname", "annotation",
-#     "analysis," "description", "type"
+#  annot - SnpAnnotationDataFrame
 #  dir - directory for output (default current)
 
-dbgap.snp.annotation <- function(annot, vardata, dir=".") {
+dbgapSnpAnnotation <- function(snpAnnot, dir=".",
+   annotationCol="dbGaP.annot", analysisCol="dbGaP.anal") {
 
+  stopifnot(is(snpAnnot, "SnpAnnotationDataFrame"))
+  stopifnot(all(c(annotationCol, analysisCol) %in% names(varMetadata(snpAnnot))))
+  
   # annotation and analysis
   for (type in c("annotation", "analysis")) {
     
     # output files
     annotfile <- paste(dir, "/SNP_", type, ".csv", sep="")
     ddfile <- paste(dir, "/SNP_", type, "_DD.txt", sep="")
-
+    
     # variables to include in sample annotation file
-    sann <- vardata$varname[vardata[,type] == 1]
-    nsann <- vardata$newname[vardata[,type] == 1]
+    if (type == "annotation") thistype <- annotationCol else thistype <- analysisCol
+    sann <- varLabels(snpAnnot)[varMetadata(snpAnnot)[[thistype]]]
 
     # keep selected variables
-    annot2 <- annot[, sann]
-
-    # change var names to a more generic form
-    names(annot2) <- nsann
+    annot2 <- pData(snpAnnot)[, sann]
 
     # prepare data dictionary
-    dd <- vardata[vardata[,type] == 1, c("newname", "type", "description")]
-    stopifnot(names(annot2) == dd$newname)
-    names(dd)[1] <- "variable"
+    meta <- varMetadata(snpAnnot)[sann,]
+    meta$type <- unlist(lapply(annot2, class))
+    meta$variable <- row.names(meta)
+    dd <- meta[,c("variable", "labelDescription", "type")]
+    names(dd) <- c("variable", "description", "type")
 
     # write the files
     write.csv(annot2, file=annotfile, quote=FALSE, row.names=FALSE)
