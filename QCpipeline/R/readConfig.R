c80dbc7d9a74aa1b655e8b94d720bb5f4e461071
Stephanie M. Gogarten <sdmorris@uw.edu>
2012-04-18 18:10:58 +0000

    added setConfigDefaults

--- QCpipeline/R/readConfig.R
+++ QCpipeline/R/readConfig.R
@@ -6,3 +6,35 @@ readConfig <- function(file, ...) {
   config[config %in% "\\t"] <- "\t"
   return(config)
 }
+
+setConfigDefaults <- function(config, required, optional, default) {
+  stopifnot(length(optional) == length(default))
+  
+  config.params <- names(config)
+  found.params <- intersect(config.params, c(required, optional))
+  if (length(found.params) > 0) {
+    message("found parameters: ", paste(found.params, collapse=", "))
+  }
+  
+  # if required params not in config, stop
+  missing.params <- setdiff(required, config.params)
+  if (length(missing.params) > 0) {
+    stop("missing required parameters: ", paste(missing.params, collapse=", "))
+  }
+
+  # if not in config, set default value
+  set.params <- setdiff(optional, config.params)
+  if (length(set.params) > 0) {
+    config[set.params] <- default[match(set.params, optional)]
+    message("using default values: ", paste(set.params, collapse=", "))
+  }
+  
+  # note unsed params in config
+  extra.params <- setdiff(config.params, c(required, optional))
+  if (length(extra.params) > 0) {
+    message("unused parameters: ", paste(extra.params, collapse=", "))
+  }
+  
+  # return config with default values set
+  return(config)
+}
