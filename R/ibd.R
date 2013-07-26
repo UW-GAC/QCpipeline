416abeec4fa2881e432dfa759209bcac187e4ab3
Adrienne Stilp <amstilp@uw.edu>
2013-07-26 21:38:28 +0000

    Added multithreading to ibd pipeline

--- R/ibd.R
+++ R/ibd.R
@@ -4,6 +4,7 @@
 ##########
 
 library(GWASTools)
+#library(QCpipeline)
 library(QCpipeline)
 library(SNPRelate)
 sessionInfo()
@@ -20,6 +21,12 @@ optional <- c("annot_scan_familyCol", "ibd_method", "out_ibd_file",
 default <- c(NA, "MoM", "ibd.RData", "ibd_kc32.RData", NA)
 config <- setConfigDefaults(config, required, optional, default)
 print(config)
+
+# multithreading on pearson?
+nSlots <- Sys.getenv("NSLOTS")
+nThreads <- ifelse(is.na(strtoi(nSlots) >= 1), 1, strtoi(nSlots))
+print(paste("Running with", nThreads,"thread(s)."))
+
 
 snp.ids <- getobj(config["out_snp_file"])
 length(snp.ids)
@@ -45,12 +52,12 @@ if (!is.na(config["annot_scan_familyCol"])) {
 
 gdsobj <- openfn.gds(config["gds_geno_file"])
 if (config["ibd_method"] == "MoM") {
-  ibd <- snpgdsIBDMoM(gdsobj, sample.id=scan.ids, snp.id=snp.ids)
+  ibd <- snpgdsIBDMoM(gdsobj, sample.id=scan.ids, snp.id=snp.ids, num.thread=nThreads)
 } else if (config["ibd_method"] == "MLE") {
-  ibd <- snpgdsIBDMLE(gdsobj, sample.id=scan.ids, snp.id=snp.ids,
+  ibd <- snpgdsIBDMLE(gdsobj, sample.id=scan.ids, snp.id=snp.ids, num.thread=nThreads,
                       method="EM")
 } else if (config["ibd_method"] == "KING") {
-  ibd <- snpgdsIBDKING(gdsobj, sample.id=scan.ids, snp.id=snp.ids,
+  ibd <- snpgdsIBDKING(gdsobj, sample.id=scan.ids, snp.id=snp.ids, num.thread=nThreads,
                        family.id=family)
 } else {
   stop("ibd method not recognized")
