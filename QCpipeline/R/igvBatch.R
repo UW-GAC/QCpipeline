724fd8d57e0ac717ca6b3fca02b71f07aed273cd
Stephanie M. Gogarten <sdmorris@uw.edu>
2013-09-19 19:08:23 +0000

    igvBatch can plot collapsed and expanded views at same time

--- QCpipeline/R/igvBatch.R
+++ QCpipeline/R/igvBatch.R
@@ -1,7 +1,9 @@
 igvBatch <- function(chromosome, position, window=10,
                      bam, outdir="./", prefix="igv",
-                     file="batch.igv", genome="hg19", collapse=FALSE) {
+                     file="batch.igv", genome="hg19",
+                     collapse=c("yes", "no", "both")) {
   stopifnot(length(chromosome) == length(position))
+  collapse <- match.arg(collapse)
 
   con <- file(file, "w")
   out <-  c("new",
@@ -14,9 +16,15 @@ igvBatch <- function(chromosome, position, window=10,
     out <- c(paste("goto chr", chromosome[i], ":", position[i]-window, "-",
                    position[i]+window, sep=""),
              "sort position")
-    if (collapse) out <- c(out, "collapse")
-    out <- c(out, paste("snapshot ", prefix, "_chr", chromosome[i], "_",
-                        position[i], ".png", sep=""))
+    if (collapse %in% c("no", "both")) {
+      out <- c(out, paste("snapshot ", prefix, "_chr", chromosome[i], "_",
+                          position[i], "_expand.png", sep=""))
+    }
+    if (collapse %in% c("yes", "both")) {
+      out <- c(out, "collapse")
+      out <- c(out, paste("snapshot ", prefix, "_chr", chromosome[i], "_",
+                          position[i], "_collapse.png", sep=""))
+    }
     writeLines(out, con)
   }
   close(con)
