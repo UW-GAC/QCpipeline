9d05f44ce2c26c04b8e1bf7b362687d63c8cf559
Adrienne Stilp <amstilp@uw.edu>
2013-11-18 18:52:17 +0000

    fixed allele_freq.R to use nc_geno_subj_file if provided, otherwise nc_geno_file

--- R/allele_freq.R
+++ R/allele_freq.R
@@ -14,14 +14,19 @@ config <- readConfig(args[1])
 
 # check config and set defaults
 required <- c("annot_scan_file", "nc_geno_file")
-optional <- c("out_afreq_file", "scan_exclude_file")
-default <- c("allele_freq.RData", NA)
+optional <- c("out_afreq_file", "scan_exclude_file", "nc_geno_subj_file")
+default <- c("allele_freq.RData", NA, NA)
 config <- setConfigDefaults(config, required, optional, default)
 print(config)
 
 
-# ncdf could be subject level
-data <- GenotypeReader(config["nc_geno_file"])
+# ncdf could be subject level - read in the subject-level if it's given
+if (!is.na(config["nc_geno_subj_file"])){
+  data <- GenotypeReader(config["nc_geno_subj_file"])
+} else {
+  data <- GenotypeReader(config["nc_geno_file"])
+}
+data
 scanAnnot <- getobj(config["annot_scan_file"])
 # take subset of annotation to match netCDF
 scanAnnot <- scanAnnot[match(getScanID(data), getScanID(scanAnnot)), ]
