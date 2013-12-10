af926f3ece81844fe60a77a983289e201274479d
Adrienne Stilp <amstilp@uw.edu>
2013-12-10 19:38:12 +0000

    updated boxplotMeanSD to add ylim functionality

--- QCpipeline/R/boxplotMeanSD.R
+++ QCpipeline/R/boxplotMeanSD.R
@@ -1,4 +1,4 @@
-boxplotMeanSD <- function(x, y, data=NULL, xlab=NULL, ylab=NULL, nSD=1, showLegend=TRUE, ...)
+boxplotMeanSD <- function(x, y, data=NULL, xlab=NULL, ylab=NULL, ylim=NULL, nSD=1, showLegend=TRUE, ...)
 {
   # get data
   if (!is.null(data))
@@ -20,12 +20,14 @@ boxplotMeanSD <- function(x, y, data=NULL, xlab=NULL, ylab=NULL, nSD=1, showLege
   maxy <- max(c(y, means+stdev), na.rm=T)
   maxy <- miny + 1.2 * (maxy-miny)
 
+  if (is.null(ylim)) ylim <- c(miny, maxy)
+  
   # boxplots
   bp <- boxplot(y~x, plot=F)
-  bxp(bp, xlim=c(0.5, nlev + 0.5), ylim=c(miny, maxy), xlab=xlab, ylab=ylab, ...)
+  bxp(bp, xlim=c(0.5, nlev + 0.5), ylim=ylim, xlab=xlab, ylab=ylab, ...)
   # add mean & SD
   par(new=TRUE)
-  plotCI(x=means, uiw=stdev, col="red", xlim=c(0.5,nlev + 0.5), ylim=c(miny, maxy), xlab="", ylab="", xaxt="n")
+  plotCI(x=means, uiw=stdev, col="red", xlim=c(0.5,nlev + 0.5), ylim=ylim, xlab="", ylab="", xaxt="n")
   if (showLegend) legend("top", c(paste("mean +/-", nSD, "SD"), "boxplot"), col = c("red", "black"), lty=c(1,1), lwd=1) 
 }
 
