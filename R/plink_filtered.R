c3919bf1c5aee0a033be2597489ff4061e8c3f8b
Adrienne Stilp <amstilp@uw.edu>
2014-03-05 00:56:19 +0000

    updated plink_filtered to deal with the fact that subj.plink may not match the subject-level netcdf

--- R/plink_filtered.R
+++ R/plink_filtered.R
@@ -22,23 +22,32 @@ config <- setConfigDefaults(config, required, optional, default)
 print(config)
 
 scanAnnot <- getobj(config["annot_scan_file"])
-scanAnnot <- scanAnnot[scanAnnot$subj.plink,]
+data <- GenotypeReader(config["nc_subj_geno_file"])
+# take subset to match gds file -- some subjects in the subject level may have been dropped later
+scanAnnot <- scanAnnot[match(getScanID(data), getScanID(scanAnnot)), ]
 snpAnnot <- getobj(config["annot_snp_file"])
 # remake snpAnnot object with alleles A and B
 snpAnnot <- SnpAnnotationDataFrame(pData(snpAnnot),
                                    alleleACol=config["annot_snp_alleleACol"],
                                    alleleBCol=config["annot_snp_alleleBCol"])
-data <- GenotypeReader(config["nc_subj_geno_file"])
+
 genoData <- GenotypeData(data, scanAnnot=scanAnnot, snpAnnot=snpAnnot)
+
+# exclude additional scans that aren't subj.plink
+#sel <- !scanAnnot$subj.plink
+scan_exclude <- scanAnnot$scanID[!scanAnnot$subj.plink]
+length(scan_exclude)
 
 ped <- paste(config["out_plink_prefix"], "_filtered", sep="")
 plinkWrite(genoData, pedFile=ped,
            individual.col=config["annot_scan_subjectCol"],
-           rs.col=config["annot_snp_rsIDCol"])
+           rs.col=config["annot_snp_rsIDCol"],
+           scan.exclude=scan_exclude)
 
 log <- paste(config["out_log_prefix"], "_filtered.log", sep="")
 res <- plinkCheck(genoData, pedFile=ped, logFile=log,
            individual.col=config["annot_scan_subjectCol"],
-           rs.col=config["annot_snp_rsIDCol"])
+           rs.col=config["annot_snp_rsIDCol"],
+           scan.exclude=scan_exclude)
 # indicate an error if check was not successful
 stopifnot(res)
