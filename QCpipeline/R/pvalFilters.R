aaa0493fca21ffd9a08943c57b8335d552972edf
Adrienne Stilp <amstilp@uw.edu>
2014-04-23 17:05:11 +0000

    added ... arguments in pvalFilters functions

--- QCpipeline/R/pvalFilters.R
+++ QCpipeline/R/pvalFilters.R
@@ -10,7 +10,7 @@ quadSolveMAF <- function(X, N) {
 
 
 ## filters - named list with 4 sets of filters (names are plot titles) (can have < 4)
-qqPlotPng <- function(pval, filters, outfile) {
+qqPlotPng <- function(pval, filters, outfile, ...) {
   png(outfile, width=720, height=720)
   par(mfrow=c(2,2), mar=c(5,5,4,2)+0.1, lwd=1.5,
       cex.axis=1.5, cex.lab=1.5, cex.sub=1.5, cex.main=1.5)    
@@ -19,20 +19,20 @@ qqPlotPng <- function(pval, filters, outfile) {
     title <- names(filters)[i]
     lambda <- median(-2*log(pval[filt]), na.rm=TRUE) / 1.39
     subtitle <- paste("lambda =", format(lambda, digits=4, nsmall=3))
-    qqPlot(pval[filt], truncate=FALSE, main=title, sub=subtitle)
+    qqPlot(pval[filt], truncate=FALSE, main=title, sub=subtitle, ...)
   }
   dev.off()
 }
 
 
 ## filters - named list with 3 sets of filters (names are plot titles)
-manhattanPlotPng <- function(pval, chromosome, filters, outfile) {
+manhattanPlotPng <- function(pval, chromosome, filters, outfile, ...) {
   png(outfile, width=720, height=720)
   par(mfrow=c(length(filters),1), mar=c(5,5,4,2)+0.1, lwd=1.5, cex.lab=1.5, cex.main=1.5)
   for (i in 1:length(filters)) {
     filt <- filters[[i]]
     title <- names(filters)[i]
-    manhattanPlot(p=pval[filt], chromosome=chromosome[filt], main=title)
+    manhattanPlot(p=pval[filt], chromosome=chromosome[filt], main=title, ...)
   }
   dev.off()
 }
