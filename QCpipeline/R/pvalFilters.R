172968d190face61487ad5f7498e2407f641204d
Adrienne Stilp <amstilp@uw.edu>
2014-07-03 18:09:46 +0000

    added addText argument to pvalFilters functions to put text in the upper left corner if requested

--- QCpipeline/R/pvalFilters.R
+++ QCpipeline/R/pvalFilters.R
@@ -10,7 +10,7 @@ quadSolveMAF <- function(X, N) {
 
 
 ## filters - named list with 4 sets of filters (names are plot titles) (can have < 4)
-qqPlotPng <- function(pval, filters, outfile, ncol=2, ...) {
+qqPlotPng <- function(pval, filters, outfile, ncol=2, addText="", ...) {
   nrow <- ceiling(length(filters) / ncol)
   png(outfile, width=360*ncol, height=360*nrow) # maybe need to calculate padding here too
   par(mfrow=c(nrow, ncol), mar=c(5,5,4,2)+0.1, lwd=1.5,
@@ -21,19 +21,30 @@ qqPlotPng <- function(pval, filters, outfile, ncol=2, ...) {
     lambda <- median(-2*log(pval[filt]), na.rm=TRUE) / 1.39
     subtitle <- paste("lambda =", format(lambda, digits=4, nsmall=3))
     qqPlot(pval[filt], truncate=FALSE, main=title, sub=subtitle, ...)
+    print(i)
+
+    if (i == 1){
+      mtext(side=3, line=-1, text=addText, padj=0.9, adj=0.02, outer=T, cex=1.5) 
+    }
   }
+  
   dev.off()
 }
 
-
 ## filters - named list with 3 sets of filters (names are plot titles)
-manhattanPlotPng <- function(pval, chromosome, filters, outfile, ...) {
+manhattanPlotPng <- function(pval, chromosome, filters, outfile, addText="", ...) {
   png(outfile, width=720, height=720)
   par(mfrow=c(length(filters),1), mar=c(5,5,4,2)+0.1, lwd=1.5, cex.lab=1.5, cex.main=1.5)
   for (i in 1:length(filters)) {
     filt <- filters[[i]]
     title <- names(filters)[i]
     manhattanPlot(p=pval[filt], chromosome=chromosome[filt], main=title, ...)
+    
+    if (i == 1){
+      mtext(side=3, line=-1, text=addText, padj=0.9, adj=0.02, outer=T, cex=1.5) 
+    }
+    
   }
+
   dev.off()
 }
