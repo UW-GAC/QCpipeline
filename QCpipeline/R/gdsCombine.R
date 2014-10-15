5eabc600ad458074c988e80118e292ff5f4991b9
Adrienne Stilp <amstilp@uw.edu>
2014-10-15 23:14:31 +0000

    added gdsCombine and checkGdsCombine (they will need to be updated for better object-oriented practices)

---
+++ QCpipeline/R/gdsCombine.R
@@ -0,0 +1,218 @@
+# this breaks many rules of object-oriented coding because something we need is not implemented yet, and we don't have time to implement it before we need this function. It will be fixed once getGenotypeSelection is implemented...
+gdsCombine <- function(gdsGenoList,
+                       filename,
+                       blockSize=5000,
+                       snpExcludeList=NULL,
+                       verbose=TRUE,
+                       dryRun=FALSE) {
+  
+  
+  if (is.null(names(gdsGenoList))) stop("Please supply names for gdsGenoList")
+  
+  # check that scanIDs are the same -- this is a requirement
+  scanID <- getScanID(gdsGenoList[[1]])
+  storage <- getNodeDescription(gdsGenoList[[1]], "genotype")$storage
+  miss.val <- getAttribute(gdsGenoList[[1]], "miss.val", "genotype")
+  for (x in gdsGenoList){
+    if (!allequal(scanID, getScanID(x))) stop("scanIDs are not all equal!")
+    if (!allequal(scanID, getScanID(x))) stop("storage modes are not all equal!")
+    if (!allequal(order(getSnpID(x)), 1:nsnp(x))) stop("snpIDs are not sorted!")
+    if (!allequal(order(getChromosome(x), getPosition(x)), 1:nsnp(x))) stop("snps are not sorted!")
+    if (!all.equal(miss.val, getAttribute(x, "miss.val", "genotype"))) stop("miss.vals are different!") # need all.equal for null
+  }
+  if (is.null(miss.val)) miss.val <- 3 # standard for bit2...
+  
+  # check snpExcludeList
+  if (!is.null(snpExcludeList)){
+    if (!all(names(snpExcludeList) %in% names(gdsGenoList))) stop("names(snpExcludeList) must be in names(gdsGenoList)")
+  }
+  
+  nsamp <- length(scanID)
+  
+  # extract snp information
+  snpInfoList <- list()
+  for (n in names(gdsGenoList)) {
+    x <- gdsGenoList[[n]]
+    snp.df <- data.frame(snpID.original=getSnpID(x),
+                         chromosome=getChromosome(x),
+                         position=getPosition(x),
+                         alleleA=getAlleleA(x),
+                         alleleB=getAlleleB(x), stringsAsFactors=F)
+    snp.df$dataset <- n
+    if (!all(snpExcludeList[[n]] %in% snp.df$snpID.original)){
+      stop(paste("snpExcludeList for dataset", n, "are not all in the gds file"))
+    }
+    snp.df <- snp.df[!(snp.df$snpID.original %in% snpExcludeList[[n]]),]
+    snpInfoList[[n]] <- snp.df
+  }
+  rm(snp.df)
+  
+  # hack until getGenotypeSelection is finished -- interact with the gds file directly
+  # make sure that it's stored scan x snp
+  for (n in names(gdsGenoList)){
+    gds <- gdsGenoList[[n]]@handler
+    # check dimensions for scan x snp
+    nsamp.n <- nscan(gdsGenoList[[n]])
+    nsnp.n <- nsnp(gdsGenoList[[n]])
+    if (!allequal(c(nsamp.n, nsnp.n), objdesp.gdsn(index.gdsn(gds, "genotype"))$dim)) stop("gds files must be scan x snp")
+  }
+  
+  snp <- do.call(rbind, snpInfoList)
+  snp <- snp[order(snp$chromosome, snp$position), ]
+  snp$snpID <- 1:nrow(snp)
+  snp$id <- paste(snp$dataset, snp$snpID.original) # unique id for merging later
+  
+  if (!dryRun){
+    # create the new file
+    if (file.exists(filename)) stop(paste("filename", filename, "already exists!"))
+    gfile <- createfn.gds(filename)
+    add.gdsn(gfile, "sample.id", scanID)
+    add.gdsn(gfile, "snp.id", snp$snpID)
+    add.gdsn(gfile, "snp.chromosome", snp$chromosome)
+    add.gdsn(gfile, "snp.position", snp$position)
+    add.gdsn(gfile, "snp.allele", paste(snp$alleleA, snp$alleleB, sep="/"))
+    gGeno <- add.gdsn(gfile, "genotype", valdim=c(length(scanID), nrow(snp)), storage=storage)
+    
+    # add missing value
+    put.attr.gdsn(gGeno, "missing.value", miss.val)
+  }
+  
+  nblocks <- ceiling(nrow(snp)/blockSize)
+  
+  counter <- 1
+  
+  if (verbose){
+    
+    if (dryRun){
+      message("Looping over SNPs..")
+    } else {
+      message("Writing genotypes..")
+    }
+  }
+  for (block in 1:nblocks){
+    snp.start <- counter
+    snp.end <- counter + blockSize-1
+    if (snp.end > nrow(snp)) snp.end <- nrow(snp)
+    snp.block <- snp[snp.start:snp.end, ]
+    nsnp.block <- snp.end - snp.start + 1
+    
+    if (verbose) message(paste("Block", block, "of", nblocks))
+    
+    geno <- matrix(NA, ncol=nsnp.block, nrow=nsamp)
+    
+    for (n in names(gdsGenoList)){
+      genoData <- gdsGenoList[[n]] # because we will use it later...
+      ggeno <- index.gdsn(genoData@handler, "genotype")
+      snpID <- getSnpID(genoData)
+      
+      # selection for combined set
+      sel.comb <- snp.block$dataset %in% n
+      
+      # selection for getting genotypes from the gds file
+      sel.n <- snpID %in% snp.block$snpID.original[snp.block$dataset %in% n]
+      
+      # make sure that snps are ordered properly -- there is no remapping here
+      stopifnot(allequal(snpID[sel.n], snp.block$snpID.original[snp.block$dataset %in% n]))
+      
+      if (sum(sel.n) == 0){
+        # no snps in this dataset for this block
+        next
+      }
+      
+      # read the genotypes
+      geno.set <- readex.gdsn(ggeno, sel=list(rep(TRUE, nsamp), sel.n))
+      
+      geno[, sel.comb] <- geno.set
+      ### OLD
+
+    }
+    
+    # set NAs to missing value for gds
+    geno[is.na(geno)] <- miss.val
+    
+    # write the genotype to the file
+    if (!dryRun){
+      start = c(1, snp.start)
+      count <- c(-1, snp.end-snp.start+1)
+      write.gdsn(gGeno, geno, start=start, count=count)
+    }
+    
+    counter <- snp.end + 1
+  }
+  
+  if (!dryRun){
+    closefn.gds(gfile)
+  }
+  return(snp)
+}
+
+
+
+
+# this checks sample by sample
+checkGdsCombine <- function(genoData, gdsGenoList, snp, snpExcludeList=NULL, verbose=TRUE){
+  
+  if (is.null(names(gdsGenoList))) stop("Please supply names for gdsGenoList")
+  
+  stopifnot(all(c("snpID", "dataset", "snpID.original", "id") %in% names(snp)))
+  
+  if (!setequal(names(gdsGenoList), unique(snp$dataset))) stop("names of gdsGenoList do not match snp$dataset!")
+  if (!all(names(snpExcludeList %in% names(gdsGenoList)))) stop("names of snpExcludeList are not all in gdsGenoList")
+  
+  chromosome <- getChromosome(genoData)
+  position <- getPosition(genoData)
+  alleleA <- getAlleleA(genoData)
+  alleleB <- getAlleleB(genoData)
+  
+  scanID <- getScanID(genoData)
+  
+  # check annotation data
+  for (n in names(gdsGenoList)){
+    message(paste("Checking snp and sample info for dataset", n))
+    genoData.set <- gdsGenoList[[n]]
+    
+    sel <- snp$dataset %in% n
+    sel.set <- getSnpID(genoData.set) %in% snp$snpID.original[sel]
+    
+    # snp data
+    if (!allequal(getChromosome(genoData.set)[sel.set], chromosome[sel])) stop(paste("chromosome does not match for dataset", n))
+    if (!allequal(getPosition(genoData.set)[sel.set], position[sel])) stop(paste("position does not match for dataset", n))
+    if (!allequal(getAlleleA(genoData.set)[sel.set], alleleA[sel])) stop(paste("alleleA does not match for dataset", n))
+    if (!allequal(getAlleleB(genoData.set)[sel.set], alleleB[sel])) stop(paste("alleleB does not match for dataset", n))
+    
+    # sample data
+    if (!allequal(getScanID(genoData.set), scanID)) stop(paste("scanID does not match for dataset", n))
+  }
+  message("ok!")
+  
+  # set up snp-mapping
+  snp.map <- list()
+  snp.set.map <- list()
+  for (n in names(gdsGenoList)){
+    snpID.set <- getSnpID(gdsGenoList[[n]])
+    snpID.set.incl <- setdiff(snpID.set, snpExcludeList[[n]])
+    j <- match(paste(n, snpID.set.incl), snp$id)
+    snp.map[[n]] <- j
+    k <- match(snpID.set.incl, snpID.set)
+    snp.set.map[[n]] <- k
+  }
+  
+  message("Checking genotype data...")
+  # loop over samples
+  for (i in 1:length(scanID)){
+    
+    if (verbose & (i %% 10 == 0)) message(paste("  Sample", i, "of", length(scanID)))
+    sid <- scanID[i]
+    
+    geno <- getGenotype(genoData, scan=c(i,1), snp=c(1,-1))
+    
+    # check each individual dataset
+    for (n in names(gdsGenoList)){
+      geno.set <- getGenotype(gdsGenoList[[n]], scan=c(i,1), snp=c(1,-1))
+      idx <- snp.map[[n]]
+      idx.set <- snp.set.map[[n]]
+      if (!allequal(geno.set[idx.set], geno[idx])) stop(paste("genotypes not equal for dataset", n))
+    }
+  }
+  
+}
---
+++ QCpipeline/inst/unitTests/gdsCombine_test.R
@@ -0,0 +1,327 @@
+
+.makeTestGds <- function(filename, nsamp=50, nsnp=100, scanBySnp=T, snpIDtype="diff1"){
+  
+  nchr <- 23
+  snp.df <- data.frame(chromosome=rep(1:nchr, each=nsnp), stringsAsFactors=F)
+  
+  snp.df$position <- sample(1:(nsnp*10), nsnp*nchr, replace=T)
+  snp.df <- snp.df[order(snp.df$chromosome, snp.df$position), ]
+  snp.df$alleleA <- sample(c("A", "C", "G", "T"), nrow(snp.df), replace=T)
+  snp.df$alleleB <- sample(c("A", "C", "G", "T"), nrow(snp.df), replace=T)
+  if (snpIDtype %in% "diff1"){
+    snp.df$snpID <- 1:nrow(snp.df)
+  } else if (snpIDtype %in% "any"){
+    snp.df$snpID <- sort(sample(1:(nrow(snp.df)*10), nrow(snp.df)))
+  }
+  
+
+  geno <- matrix(sample(0:3, nsnp*nchr*nsamp, replace=T), nrow=nsamp)
+  
+  if (file.exists(filename)) stop(paste("filename", filename, "already exists!"))
+  gfile <- createfn.gds(filename)
+  add.gdsn(gfile, "sample.id", 1:nsamp)
+  add.gdsn(gfile, "snp.id", snp.df$snpID)
+  add.gdsn(gfile, "snp.chromosome", snp.df$chromosome)
+  add.gdsn(gfile, "snp.position", snp.df$position)
+  add.gdsn(gfile, "snp.allele", paste(snp.df$alleleA, snp.df$alleleB, sep="/"))
+  add.gdsn(gfile, "genotype", geno, storage="bit2")
+  
+  closefn.gds(gfile)
+}
+
+test_gdsCombine <- function(){
+  
+  for (snptype in c("diff1", "any")){
+    
+    message(snptype)
+    
+    fileA <- tempfile()
+    .makeTestGds(fileA, snpIDtype=snptype)
+    gdsA <- GdsGenotypeReader(fileA)
+    
+    fileB <- tempfile()
+    .makeTestGds(fileB, snpIDtype=snptype)
+    gdsB <- GdsGenotypeReader(fileB)
+    
+    fileC <- tempfile()
+    .makeTestGds(fileC, snpIDtype=snptype)
+    gdsC <- GdsGenotypeReader(fileC)
+    
+    gdsList <- list(A=gdsA, B=gdsB, C=gdsC)
+    
+    filename <- tempfile()
+    
+    for (b in c(5000, (nsnp(gdsA) + nsnp(gdsB) + nsnp(gdsC) - 1))){
+      message(b)
+      snp <- gdsCombine(gdsList, filename, blockSize=b, dryRun=TRUE)  
+      checkTrue(!file.exists(filename))
+      snp <- gdsCombine(gdsList, filename, blockSize=b)  
+      
+      gds <- GdsGenotypeReader(filename)
+      
+      geno <- getGenotype(gds)
+      
+      # check individual datasets A
+      for (n in names(gdsList)){
+        gds.set <- gdsList[[n]]
+        sel <- snp$dataset %in% n
+        geno.set <- getGenotype(gds.set)
+        checkEquals(geno.set, geno[sel, ])
+        checkEquals(getAlleleA(gds)[sel], getAlleleA(gds.set))  
+        checkEquals(getAlleleB(gds)[sel], getAlleleB(gds.set))  
+        checkEquals(getChromosome(gds)[sel], getChromosome(gds.set))  
+        checkEquals(getPosition(gds)[sel], getPosition(gds.set))  
+        # check annotation
+        checkEquals(snp$alleleA[sel], getAlleleA(gds.set))  
+        checkEquals(snp$alleleB[sel], getAlleleB(gds.set))  
+        checkEquals(snp$chromosome[sel], getChromosome(gds.set))  
+        checkEquals(snp$position[sel], getPosition(gds.set))  
+        
+      }
+      close(gds)
+      unlink(filename)
+    }
+    
+    lapply(gdsList, close)
+
+    unlink(fileA)
+    unlink(fileB)
+    unlink(fileC)
+    
+  }
+  
+}
+
+
+test_checkGdsCombine <- function() {
+  
+  fileA <- tempfile()
+  .makeTestGds(fileA)
+  gdsA <- GdsGenotypeReader(fileA)
+  
+  fileB <- tempfile()
+  .makeTestGds(fileB)
+  gdsB <- GdsGenotypeReader(fileB)
+  
+  fileC <- tempfile()
+  .makeTestGds(fileC)
+  gdsC <- GdsGenotypeReader(fileC)
+  
+  gdsList <- list(A=gdsA, B=gdsB, C=gdsC)
+  
+  filename <- tempfile()
+  filename.fail <- tempfile()
+  
+  # combine them
+  snp <- gdsCombine(gdsList, filename)  
+  
+  # make sure it works
+  gds <- GdsGenotypeReader(filename)
+  checkGdsCombine(gds, gdsList, snp)
+  close(gds)
+  
+  # change a snp chromosome
+  file.copy(filename, filename.fail)
+  gds <- openfn.gds(filename.fail, readonly=F)
+  write.gdsn(index.gdsn(gds, "snp.chromosome"), val=30, start=1, count=1)
+  closefn.gds(gds)
+  # make sure it fails
+  genoData <- GdsGenotypeReader(filename.fail)
+  checkException(checkGdsCombine(genoData, gdsList, snp))
+  close(genoData)
+  unlink(filename.fail)
+  
+  # change a genotype
+  file.copy(filename, filename.fail)
+  gds <- openfn.gds(filename.fail, readonly=F)
+  gGeno <- index.gdsn(gds, "genotype")
+  geno <- read.gdsn(gGeno)
+  i.na <- which(geno == get.attr.gdsn(gGeno)$missing.value, arr.ind=T)
+  # change the first one to non-zero
+  write.gdsn(gGeno, val=1, start=(i.na[1, ]), count=c(1,1))
+  closefn.gds(gds)
+  # make sure it fails
+  genoData <- GdsGenotypeReader(filename.fail)
+  checkException(checkGdsCombine(genoData, gdsList, snp))
+  close(genoData)
+  unlink(filename.fail)
+  
+  # change a non-missing value to another non-missing value
+  geno.map <- c("0"=1, "1"=2, "2"=0)
+  file.copy(filename, filename.fail)
+  gds <- openfn.gds(filename.fail, readonly=F)
+  gGeno <- index.gdsn(gds, "genotype")
+  geno <- read.gdsn(gGeno)
+  i.na <- which(geno != get.attr.gdsn(gGeno)$missing.value, arr.ind=T)
+  # change the first one to non-zero
+  oldval <- geno[i.na[2, "row"], i.na[2, "col"]]
+  newval <- geno.map[as.character(oldval)]
+  write.gdsn(gGeno, val=newval, start=(i.na[2, ]), count=c(1,1))
+  closefn.gds(gds)
+  # make sure it fails
+  genoData <- GdsGenotypeReader(filename.fail)
+  checkException(checkGdsCombine(genoData, gdsList, snp))
+  close(genoData)
+  unlink(filename.fail)
+  
+  
+  lapply(gdsList, close)
+  
+  unlink(fileA)
+  unlink(fileB)
+  unlink(fileC)
+  unlink(filename)
+  
+}
+
+
+
+test_gdsCombine_snpExclude <- function(){
+  
+  snptype <- 'any'
+  
+  fileA <- tempfile()
+  .makeTestGds(fileA, snpIDtype=snptype)
+  gdsA <- GdsGenotypeReader(fileA)
+  
+  fileB <- tempfile()
+  .makeTestGds(fileB, snpIDtype=snptype)
+  gdsB <- GdsGenotypeReader(fileB)
+  
+  fileC <- tempfile()
+  .makeTestGds(fileC, snpIDtype=snptype)
+  gdsC <- GdsGenotypeReader(fileC)
+  
+  gdsList <- list(A=gdsA, B=gdsB, C=gdsC)
+  
+  # snp.exclude
+  snp.exclude <- list()
+  # do not exclude any from the first file
+  snp.exclude[["B"]] <- getSnpID(gdsB)[2]
+  snp.exclude[["C"]] <- sample(getSnpID(gdsC), 5)
+  
+  filename <- tempfile()
+  
+  snp <- gdsCombine(gdsList, filename, snpExcludeList=snp.exclude)
+  
+  gds <- GdsGenotypeReader(filename)
+  
+  geno <- getGenotype(gds)
+  
+  # check individual datasets A
+  for (n in names(gdsList)){
+    gds.set <- gdsList[[n]]
+    sel <- snp$dataset %in% n
+    sel.set <- getSnpID(gds.set) %in% snp$snpID.original[sel]
+    geno.set <- getGenotype(gds.set)
+    checkEquals(geno[sel, ], geno.set[sel.set, ])
+    checkEquals(getAlleleA(gds)[sel], getAlleleA(gds.set)[sel.set])  
+    checkEquals(getAlleleB(gds)[sel], getAlleleB(gds.set)[sel.set])  
+    checkEquals(getChromosome(gds)[sel], getChromosome(gds.set)[sel.set])  
+    checkEquals(getPosition(gds)[sel], getPosition(gds.set)[sel.set])  
+    # check annotation
+    checkEquals(snp$alleleA[sel], getAlleleA(gds.set)[sel.set])  
+    checkEquals(snp$alleleB[sel], getAlleleB(gds.set)[sel.set])  
+    checkEquals(snp$chromosome[sel], getChromosome(gds.set)[sel.set])  
+    checkEquals(snp$position[sel], getPosition(gds.set)[sel.set])  
+    
+  }
+  close(gds)
+  unlink(filename)
+  
+  lapply(gdsList, close)
+  
+  unlink(fileA)
+  unlink(fileB)
+  unlink(fileC)
+  
+  
+}
+
+
+
+test_checkGdsCombine_snpExclude <- function() {
+  
+  fileA <- tempfile()
+  .makeTestGds(fileA)
+  gdsA <- GdsGenotypeReader(fileA)
+  
+  fileB <- tempfile()
+  .makeTestGds(fileB)
+  gdsB <- GdsGenotypeReader(fileB)
+  
+  fileC <- tempfile()
+  .makeTestGds(fileC)
+  gdsC <- GdsGenotypeReader(fileC)
+  
+  gdsList <- list(A=gdsA, B=gdsB, C=gdsC)
+  
+  filename <- tempfile()
+  filename.fail <- tempfile()
+  
+  # snp exclude
+  snp.exclude <- list()
+  # do not exclude any from the first file
+  snp.exclude[["B"]] <- getSnpID(gdsB)[2]
+  snp.exclude[["C"]] <- sample(getSnpID(gdsC), 5)
+  
+  # combine them
+  snp <- gdsCombine(gdsList, filename, snpExcludeList=snp.exclude)  
+  
+  # make sure it works
+  gds <- GdsGenotypeReader(filename)
+  checkGdsCombine(gds, gdsList, snp, snpExcludeList=snp.exclude)
+  # different order for snp exclude list
+  checkGdsCombine(gds, gdsList, snp, snpExcludeList=snp.exclude[c("C", "B", "A")])
+  
+  # try running without the snpExcludeList
+  checkException(checkGdsCombine(gds, gdsList, snp))
+  
+  # try permuting the snpExcludeList
+  snp.exclude.fail <- snp.exclude
+  names(snp.exclude.fail) <-c("C", "A")
+  checkException(checkGdsCombine(gds, gdsList, snp, snpExcludeList = snp.exclude.fail))
+  close(gds)
+    
+  # change a genotype
+  file.copy(filename, filename.fail)
+  gds <- openfn.gds(filename.fail, readonly=F)
+  gGeno <- index.gdsn(gds, "genotype")
+  geno <- read.gdsn(gGeno)
+  i.na <- which(geno == get.attr.gdsn(gGeno)$missing.value, arr.ind=T)
+  # change the first one to non-zero
+  write.gdsn(gGeno, val=1, start=(i.na[1, ]), count=c(1,1))
+  closefn.gds(gds)
+  # make sure it fails
+  genoData <- GdsGenotypeReader(filename.fail)
+  checkException(checkGdsCombine(genoData, gdsList, snp, snpExcludeList = snp.exclude))
+  close(genoData)
+  unlink(filename.fail)
+  
+  # change a non-missing value to another non-missing value
+  geno.map <- c("0"=1, "1"=2, "2"=0)
+  file.copy(filename, filename.fail)
+  gds <- openfn.gds(filename.fail, readonly=F)
+  gGeno <- index.gdsn(gds, "genotype")
+  geno <- read.gdsn(gGeno)
+  i.na <- which(geno != get.attr.gdsn(gGeno)$missing.value, arr.ind=T)
+  # change the first one to non-zero
+  oldval <- geno[i.na[2, "row"], i.na[2, "col"]]
+  newval <- geno.map[as.character(oldval)]
+  write.gdsn(gGeno, val=newval, start=(i.na[2, ]), count=c(1,1))
+  closefn.gds(gds)
+  # make sure it fails
+  genoData <- GdsGenotypeReader(filename.fail)
+  checkException(checkGdsCombine(genoData, gdsList, snp, snpExcludeList = snp.exclude))
+  close(genoData)
+  unlink(filename.fail)
+  
+  
+  lapply(gdsList, close)
+  
+  unlink(fileA)
+  unlink(fileB)
+  unlink(fileC)
+  unlink(filename)
+  
+}
